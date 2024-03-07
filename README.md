38e0d98ab44375e19eefb73c21e4ae546b96ac66
kevster7000 <ktang147@gmail.com>
2024-03-07 04:29:36 -0600

    Camera Input done

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 # [**Fruit Identifier**](https://kevster7000.github.io/Fruit-Identifier/)
 
-***A machine learning program trained to identify the name of 45 different fruits from an image or camera.***
+***A machine learning program trained to identify the name of 40 different fruits from an image or camera.***
 
-<img src="./assets/light-theme.png" width="100%">
+<img src="./assets/light-theme.png" width="100%" alt="Light Theme">
 
-<img src="./assets/dark-theme.png" width="100%">
+<img src="./assets/dark-theme.png" width="100%" alt="Dark Theme">
 
-This project was made for Assignment 7 in CSCE 436, Human Computer Interaction. I had the impulsive idea to go the extra mile and make this thing for what was supposedly a simple assignment. Anywho, this was made with HTML, SASS, JavaScript, and a bit of SVGs. But the main character of this bad boy was made with [Teachable Machine](https://teachablemachine.withgoogle.com/), an easy way to create machine learning models. Though, I created this project fairly quickly (about 4 days), so it is not fully "finished" so to say. It is completely functional, but there are most likely some details and whatnot that disturb my perfectionist self enough to say it is not entirely finished. And, I don't plan on tidying the thing up or doing the nitpicky things at anytime whatsover; I'm not very attracted to that idea, and the project has gotten less appealing quite quickly. Regardless, this was a fun little side quest I embarked on whilst being slaughtered in the midst of the semester.
+This project was made for Assignment 7 in CSCE 436, Human Computer Interaction. I had the impulsive idea to go the extra mile and make this thing for what was supposedly a simple assignment. Anywho, this was made with HTML, SASS, JavaScript, and a bit of SVGs. But the main character of this bad boy was made with [Teachable Machine](https://teachablemachine.withgoogle.com/), an easy way to create machine learning models. Though, I created this project fairly quickly (about 4 days), so it is not fully "finished" so to say. It is completely functional, but there are most likely some details and whatnot that disturb my perfectionist self enough to say that it is not entirely finished. And, I don't plan on tidying the thing up or doing the nitpicky things at anytime whatsover; I'm not very attracted to that idea, and the project has gotten less appealing quite quickly. Regardless, this was a fun little side quest I embarked on whilst being slaughtered in the midst of the semester.
--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 document.addEventListener("DOMContentLoaded", initApp);
-function initApp() {
+async function initApp() {
 
 /**********************************************************************************/
 /*                                  BG Gradient                                   */
@@ -87,24 +87,32 @@ function showError() {
 /**********************************************************************************/
 
 let webcam;
-let webcamContainer = document.getElementById("webcam-container");
+let uploadContainer = document.querySelector(".upload-container");
 let cameraBtn = document.querySelector(".camera-btn");
 
-cameraBtn.addEventListener("click", async () => {
+cameraBtn.addEventListener("click", initCamera);
+
+async function initCamera() {
+    await webcamUI();
+    const webcamContainer = uploadContainer.querySelector("#webcam");
+
     const flip = true;
-    const width = 200;
-    const height = 200;
+    const width = webcamContainer.offsetWidth;
+    const height = webcamContainer.offsetHeight;
     webcam = new tmImage.Webcam(width, height, flip);
     await webcam.setup();
     await webcam.play();
-    webcamContainer.appendChild(webcam.canvas);
+    
+    uploadContainer.querySelector("#webcam").appendChild(webcam.canvas);
     window.requestAnimationFrame(loop);
-});
+}
 
 async function loop() {
     webcam.update();
-    // await predictCamera();
-    window.requestAnimationFrame(loop);
+    await predictCamera();
+    setTimeout(() => {
+        window.requestAnimationFrame(loop);
+    }, 120);
 }
 
 
@@ -112,16 +120,16 @@ async function loop() {
 /*                                Teachable Machine                               */
 /**********************************************************************************/
 
-const URL = "./my_model/";
-let model, labelContainer, maxPredictions;
+const URL = "https://teachablemachine.withgoogle.com/models/buZqURmyD/";
+const maxPredictions = 10;
 const modelURL = URL + "model.json";
 const metadataURL = URL + "metadata.json";
-
-let isIos = false; 
-if (window.navigator.userAgent.indexOf('iPhone') > -1 || window.navigator.userAgent.indexOf('iPad') > -1) isIos = true;
+let model;
 
 async function predictImage(image) {
+    await initModel();
     let prediction = await model.predict(image);
+
     for (let i = 0; i < maxPredictions; i++) {
         const classPrediction = prediction[i].className + ": " + prediction[i].probability.toFixed(2);
         labelContainer.childNodes[i].textContent = classPrediction;
@@ -129,26 +137,34 @@ async function predictImage(image) {
 }
 
 async function predictCamera() {
-    let prediction;
-    if (isIos) prediction = await model.predict(webcam.webcam);
-    else prediction = await model.predict(webcam.canvas);
+    await initModel();
+    let prediction = await model.predictTopK(webcam.canvas, maxPredictions);
+
+    let labelContainer = document.getElementById("label-container");
+    labelContainer.innerHTML = "";
+
     for (let i = 0; i < maxPredictions; i++) {
-        const classPrediction = prediction[i].className + ": " + prediction[i].probability.toFixed(2);
-        labelContainer.childNodes[i].textContent = classPrediction;
+        const name = prediction[i].className;
+        const percent = (prediction[i].probability * 100);
+        labelContainer.innerHTML += `<li style="--FRUIT-PERCENT: ${percent.toFixed(2)}%; --FRUIT-COLOR: var(--${name.toUpperCase().replaceAll(" ", "-")})"><span>${name}: ${Math.round(percent)}%</span></li>`;
     }
 }
 
-async function initPrediction() {
+async function initModel() {
     model = await tmImage.load(modelURL, metadataURL);
-    maxPredictions = model.getTotalClasses();
 }
 
-function initialUI() {
+
+/**********************************************************************************/
+/*                                    UI States                                   */
+/**********************************************************************************/
+
+async function initialUI() {
     const content = `
         <span><h2>Upload Image</h2></span>
         <p class="error-message">Image type not supported</p>
         <div class="upload-btn-container">
-            <button class="upload-btn">Upload Image</button>
+            <button class="upload-btn" type="button">Upload Image</button>
             <input id="getFile" name="getFile" type="file" accept=".jpg, .jpeg, .png, .svg">
             <div class="divider">
                 <p><span>or</span></p>
@@ -156,15 +172,56 @@ function initialUI() {
         </div>
         <div class="camera-btn-container">
             <button class="camera-btn" type="button">Use Camera</button>
-        </div>`;
+        </div>
+    `;
 
+    uploadContainer.innerHTML = content;
+
+    let uploadBtn = document.querySelector(".upload-btn");
+    let inputFile = document.getElementById('getFile');
+    let cameraBtn = document.querySelector(".camera-btn");
+
+    uploadBtn.addEventListener("click", () => {
+        inputFile.click();
+    });
+
+    inputFile.addEventListener("change", () => {
+        checkFile(inputFile.files[0]);
+    });
+
+    cameraBtn.addEventListener("click", initCamera);
 }
 
 function resultUI() {
 
 }
 
-function webcamUI() {
+async function webcamUI() {
+    const content = `
+        <span><h2>Upload Image</h2></span>
+        <div class="webcam-header">
+            <button type="button">
+                <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" viewBox="0 0 100 100">
+                    <line x1="20" x2="80" y1="50" y2="50" stroke-width="8" stroke-linejoin="round" stroke-linecap="round" />
+                    <line x1="20" x2="20" y1="20" y2="50" stroke-width="8" stroke-linejoin="round" stroke-linecap="round" transform="rotate(45, 20, 50)"/>
+                    <line x1="20" x2="20" y1="20" y2="50" stroke-width="8" stroke-linejoin="round" stroke-linecap="round" transform="rotate(135, 20, 50)"/>
+                </svg>
+                <p>Back</p>
+            </button>
+        </div>
+        <div class="webcam-body">
+            <div id="webcam"></div>
+            <ol id="label-container">
+            </ol>
+        </div>
+    `;
 
+    uploadContainer.innerHTML = content;
+
+    let backBtn = document.querySelector(".webcam-header button");
+    backBtn.addEventListener("click", async () => {
+        webcam.stop();
+        await initialUI();
+    });
 }
 }
